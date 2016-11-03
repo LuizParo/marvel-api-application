acf46af75c673f56553e0d4222d42a584e94b478
flpereira <flpereira@cit017014.cit>
2016-11-03 15:00:52 -0200

    Adicionando código inicial.

---
+++ src/main/java/br/com/treinamento/dojo/service/MarvelRequestSender.java
@@ -0,0 +1,14 @@
+package br.com.treinamento.dojo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by diegob on 27/10/2016.
+ */
+@Service
+public class MarvelRequestSender {
+
+    private static final String publicKey = "1acd3b921cc928af72236227fcc1fadd";
+    private static final String privateKey = "2db991eeff8833acdecf83d663f416f66c89ffe6";
+
+}
